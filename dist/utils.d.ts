fb7c6d021cd7070cacc62a694dbbb61917dde29d
Radek Šerý <radek.sery@peckadesign.cz>
2023-02-10 11:19:11 +0100

    chore: github action for building and publishing package to npm
    Built files in dist folder are no longer commited, but are built automatically when new release is made on github.

--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export type DeepPartial<T> = T extends object ? {
-    [P in keyof T]?: DeepPartial<T[P]>;
-} : T;
-export declare function mergeDeep(target: any, ...sources: any): Record<string, unknown>;
-export declare let caUid: number;
-export declare function getUid(): string;
