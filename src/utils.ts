2f967c573629d1dbf7549c4b977325b1ad8d648e
Radek Šerý <radek.sery@peckadesign.cz>
2023-04-11 09:48:19 +0200

    feature: ESM version exports `deepMerge` function
    `deepMerge` function used by the package for deep merging multiple objects is now exported.

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ function isObject(item: unknown) {
 	return item && typeof item === 'object' && !Array.isArray(item)
 }
 
-export function mergeDeep(target: any, ...sources: any): Record<string, unknown> {
+export function deepMerge(target: any, ...sources: any): Record<string, unknown> {
 	if (!sources.length) return target
 	const source = sources.shift()
 
@@ -14,14 +14,14 @@ export function mergeDeep(target: any, ...sources: any): Record<string, unknown>
 		for (const key in source) {
 			if (isObject(source[key])) {
 				if (!target[key]) Object.assign(target, { [key]: {} })
-				mergeDeep(target[key], source[key])
+				deepMerge(target[key], source[key])
 			} else {
 				Object.assign(target, { [key]: source[key] })
 			}
 		}
 	}
 
-	return mergeDeep(target, ...sources)
+	return deepMerge(target, ...sources)
 }
 
 // UID for CollapsableItem
