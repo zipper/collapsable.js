e5b7303b1a688afcf5b9b1cbda265dc141e7217e
Radek Šerý <radek.sery@peckadesign.cz>
2023-11-01 14:07:37 +0100

    feature: Control element is now optional
    The control element for `CollapsableItem` is now optional. If there is no control element, you can either expand / collapse the item programmatically from JS, or you can use an external link.

--- src/CollapsableItem.ts
+++ src/CollapsableItem.ts
@@ -41,7 +41,7 @@ export class CollapsableItem {
 		const controlElements = element.querySelectorAll<HTMLElement>(collapsable.options.control)
 		const boxElements = element.querySelectorAll<HTMLElement>(collapsable.options.box)
 
-		if (!controlElements.length || !boxElements.length) {
+		if (!boxElements.length) {
 			throw new Error(`Collapsable: Missing control or box element.'`)
 		}
 
